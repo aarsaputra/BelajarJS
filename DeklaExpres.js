45f9f1647cd7f6d4550ad284ed18279149dee529
aarsaputra <aarsaputra03@gmail.com>
2023-09-10 09:47:54 +0800

    menambahkan funtion declartion dan expression

---
+++ DeklaExpres.js
@@ -0,0 +1,12 @@
+// function decleration dan expression
+// Contoh function dengan decleration
+function PopUp (Aar){
+  alert('Halo ' + Aar);
+  return;
+}
+PopUp('Aar');
+//Contoh funtion dengan expression
+var pop = function(hei){
+  alert('ngetes function expression ' + hei)
+}
+pop('heiiii')
